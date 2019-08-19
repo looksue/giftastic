ddadfe94d9c3fd3ed2f5956f5a58aa4f8b169bec
Sue Look <suelook1@gmail.com>
2019-08-19 10:36:43 -0400

    created api and event listener

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+var topics = ["cat", "dog", "bird","horses","capybara"];
+$("button").on("click", function(){
+    var animal= $(this).attr("data-animal");
+
+//constructing query
+var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ animal + "&api_key=4vsBrisJcwIs9UYSnj8pkvZjsjBs3d0X&limit=10";
+
+//AJAX request with query
+$.ajax ({
+    url:queryURL,
+    method: "GET"
+})
+.then (function(response){
+    console.log (queryURL);
+})
+
+//storing the data from request
+var results= response.data; 
+
+//looping through results
+for (var i=0; i<results.length; i++){
+    }
+
+//create and store results in div
+var animalDiv = $("<div>");
+
+//create a p tag with items rating
+var p = $ ("<p>").text ("Rating: " + results[i].rating);
+
+//create and store image tag
+var animalImage = 4("<img>");
+
+//set src attr of image to a property pulled off result item, is fixed height the attr?
+animalImage.attr("src", results[i].images.fixed_height.url);
+
+//append the p and img tag to animal div
+animalDiv.append(p);
+animalDiv.append(animalImage);
+
+//prepend the animaldiv to html page 
+$("#gifs-appear-here").prepend(animalDiv);
+
+})
