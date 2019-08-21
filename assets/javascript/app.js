2e2d35a36726db0c57b5957a59d24a5b0331eec3
Sue Look <suelook1@gmail.com>
2019-08-21 14:29:36 -0400

    api call working and gifs displayed and styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,36 +22,36 @@ $("button").on("click", function () {
         url: queryURL,
         method: "GET"
     })
-        .then(function (response) {
+        .then(function(response) {
             console.log(queryURL);
+
+            //storing the data from request
+            var results = response.data;
+
+            //looping through results
+            for (var i = 0; i < results.length; i++) {
+
+                //create and store results in div
+                var animalDiv = $("<div>");
+                animalDiv.css("display", "inline-block");
+                //create a p tag with items rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
+
+                //create and store image tag
+                var animalImage = $("<img>");
+
+                //set src attr of image to a property pulled off result item, is fixed height the attr?
+                animalImage.attr("src", results[i].images.fixed_height.url);
+
+                //append the p and img tag to animal div
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+
+                //prepend the animaldiv to gifs container 
+                 $("#gifs-appear-here").prepend(animalDiv);
+        }
+
         })
-
-    //storing the data from request
-    var results = response.data;
-
-    //looping through results
-    for (var i = 0; i < results.length; i++) {
-    }
-
-    //create and store results in div
-    var animalDiv = $("<div>");
-
-    //create a p tag with items rating
-    var p = $("<p>").text("Rating: " + results[i].rating);
-
-    //create and store image tag
-    var animalImage = 4("<img>");
-
-    //set src attr of image to a property pulled off result item, is fixed height the attr?
-    animalImage.attr("src", results[i].images.fixed_height.url);
-
-    //append the p and img tag to animal div
-    animalDiv.append(p);
-    animalDiv.append(animalImage);
-
-    //prepend the animaldiv to html page 
-    $("#gifs-appear-here").prepend(animalDiv);
-
 })
 
 // event listener for the new animal button
